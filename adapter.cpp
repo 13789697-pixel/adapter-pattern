8657b965f34b62d3ec70c689fc1524cc3d48167f
Серьоженко Дмитро <13789697@gsuite.duit.edu.ua>
2025-11-20 16:21:28 +0200

    Add files via upload

---
+++ adapter.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+// Цільовий інтерфейс //
+class IMessage {
+public:
+    virtual void send(const string& text) = 0;
+    virtual ~IMessage() = default;
+};
+
+// Стара реалізація //
+class LegacyPrinter {
+public:
+    void printText(const string& text) {
+        cout << "LegacyPrinter: " << text << endl;
+    }
+};
+
+// адаптер //
+class PrinterAdapter : public IMessage {
+private:
+    LegacyPrinter* legacy;
+public:
+    PrinterAdapter(LegacyPrinter* lp) : legacy(lp) {}
+
+    void send(const string& text) override {
+        // викликаємо старий метод через адаптер//
+        legacy->printText(text);
+    }
+};
+
+int main() {
+    //старий принтер//
+    LegacyPrinter oldPrinter;
+    PrinterAdapter adapter(&oldPrinter);
+    adapter.send("Hello from the adapter pattern!");
+
+    return 0;
+}
